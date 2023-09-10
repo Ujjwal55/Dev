debbe46487acf3b4f6c7e1d9d0ec01736e286304
ujjwalS2C <ujjwal.a@survey2connect.com>
2023-09-10 10:45:57 +0530

    feat: added cors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const path = require('path')
-const app = express()
+const app = express();
+const cors = require("cors");
 
 connectDB()
 
@@ -14,6 +15,8 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
     //set static folder
